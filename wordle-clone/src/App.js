5d2f011c43fbee32ae3b52872b13c06ad80870be
richbruzzese <richbruzzese@gmail.com>
2024-05-16 15:31:18 -0400

    comments and thoughts on approach

--- wordle-clone/src/App.js
+++ wordle-clone/src/App.js
@@ -10,10 +10,13 @@ function App() {
   },[])
   
   // create an input that will accept a word
-  // compare to the letters within the set word
+  // compare to the letters within the set word - ez map
+  // map input - maybe set state for guess - possibly unnecessary?
+  // compare index for each input against index for each randWord
   // determine if letters match
-  // highlight which match and which dont
-  // x number of tries before death
+  // highlight which match and which dont (will probably start with just an x/x match)
+  // x number of tries before death - another piece of state for tries - could just use a var, but setting it in state is cleaner imo
+  // tries < 5 ? render app : you lose
  const checkWords = () => {
   // use this to check words
  }
